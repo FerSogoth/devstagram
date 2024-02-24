31d040adfa3c37abbf871be6852faa74f4dbbbc2
FerSogoth <penella.garcia.fernando@gmail.com>
2024-02-24 20:07:17 +0100

    first commit

---
+++ app/Http/Controllers/ComentarioController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ComentarioController extends Controller
+{
+    public function store(Request $request, User $user, Post $post) {
+        
+        $this->validate($request, [
+            'comentario' => 'required|max:255'
+        ]);
+
+        //Crear comentario
+
+        Comentario::create([
+            'comentario' => $request->comentario,
+            'post_id' => $post->id,
+            'user_id' => auth()->user()->id
+        ]); 
+
+        return back()->with('mensaje', 'Comentario realizado correctamente');
+
+    }
+}
